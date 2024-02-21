d7c6f53191f8e6f5e42ddaafb89a85fe33e1f959
ray <ppt361551@gmail.com>
2024-02-21 10:36:04 +0800

    2

--- src/Main.java
+++ src/Main.java
@@ -20,25 +20,58 @@ public class Main extends JPanel implements KeyListener {
     private int speed = 100;
     private static String direction;
     private boolean allowKeyPress;
+    private int score;
 
     public Main() {
-        snake = new Snake();
-        fruit = new Fruit();
+        reset();
+        addKeyListener(this);
+    }
+
+    private void setTimer() {
         t = new Timer();
         t.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-            repaint();
+                repaint();
             }
         },0, speed);
-        direction = "Right";
-        addKeyListener(this);
+    }
+
+    private void reset() {
+        score = 0;
+        if (snake != null) {
+            snake.getSnakeBody().clear();
+        }
         allowKeyPress = true;
+        direction = "Right";
+        snake = new Snake();
+        fruit = new Fruit();
+        setTimer();
     }
 
     @Override
     public void paintComponent(Graphics g) {
-//        System.out.println("We are calling paintComponent...");
+        ArrayList<Node> snake_body = snake.getSnakeBody();
+        Node head = snake_body.get(0);
+        for (int i = 1; i < snake_body.size(); i++){
+            if (snake_body.get(i).x == head.x && snake_body.get(i).y == head.y) {
+                allowKeyPress = false;
+                t.cancel();
+                t.purge();
+                int response = JOptionPane.showOptionDialog(this, "Game Over! Would you like to start over?", "Game Over", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, JOptionPane.YES_OPTION);
+                switch (response) {
+                    case JOptionPane.CLOSED_OPTION:
+                        System.exit(0);
+                        break;
+                    case JOptionPane.NO_OPTION:
+                        System.exit(0);
+                        break;
+                    case JOptionPane.YES_OPTION:
+                        reset();
+                        return;
+                }
+            }
+        }
 
         g.fillRect(0, 0, width, height);
         fruit.drawFruit(g);
